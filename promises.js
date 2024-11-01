5cedd9bdf6bb5755cb53d91d2a58a80690ea8633
Ash digit <ashdigit44@gmail.com>
2024-11-01 21:37:19 +0000

    played a little bit more with the promises

--- promises.js
+++ promises.js
@@ -1,24 +1,37 @@
-function aPromiseFunction() {
+function walkTheDog() {
   return new Promise((resolved, rejected) => {
     setTimeout(() => {
-      resolved("a Promise has been resolved");
+      resolved("I walked the Dog.");
     }, 1500);
   });
 }
 
-function anotherPromiseFunction() {
+function cleanTheHouse() {
   return new Promise((resolved, rejected) => {
     setTimeout(() => {
-      resolved("this is the other Promise from anotherPromiseFunction");
+      resolved("I cleaned the House");
     }, 1000);
   });
 }
 
-aPromiseFunction()
-  .then((resolved) => {
-    console.log(resolved);
-    return anotherPromiseFunction();
+function buyGraphicsCard() {
+  return new Promise((resolved, rejected) => {
+    setTimeout(() => {
+      resolved("I just bought a 4090!");
+    }, 3000);
+  });
+}
+
+walkTheDog()
+  .then((dogThing) => {
+    console.log(dogThing);
+    return cleanTheHouse();
   })
-  .then((value) => {
-    console.log(value);
+  .then((houseThing) => {
+    console.log(houseThing);
+    return buyGraphicsCard();
+  })
+  .then((gpuThing) => {
+    console.log(gpuThing);
+    console.log("I've done too much! need some refreshment!");
   });
