cc861131281f3f3f2654adb3aec4428d849e5da3
Ash digit <ashdigit44@gmail.com>
2024-11-01 22:02:34 +0000

    worked with promise rejections a little bit

--- promises.js
+++ promises.js
@@ -1,7 +1,15 @@
+//this is just a simple example of Promise object
+//I call this block of code: part ONE and this is the begining of it
+
 function walkTheDog() {
   return new Promise((resolved, rejected) => {
     setTimeout(() => {
-      resolved("I walked the Dog.");
+      const isTheDogWalked = true;
+      if (isTheDogWalked) {
+        resolved("I walked the Dog.");
+      } else {
+        rejected("Nope You didn't walk the dog, How dare you!");
+      }
     }, 1500);
   });
 }
@@ -9,7 +17,12 @@ function walkTheDog() {
 function cleanTheHouse() {
   return new Promise((resolved, rejected) => {
     setTimeout(() => {
-      resolved("I cleaned the House");
+      const isCleaned = true;
+      if (isCleaned) {
+        resolved("I cleaned the House");
+      } else {
+        rejected("You didn't clean the house!");
+      }
     }, 1000);
   });
 }
@@ -17,7 +30,12 @@ function cleanTheHouse() {
 function buyGraphicsCard() {
   return new Promise((resolved, rejected) => {
     setTimeout(() => {
-      resolved("I just bought a 4090!");
+      const isBought = false;
+      if (isBought) {
+        resolved("I just bought a 4090!");
+      } else {
+        rejected("Nope, You did not buy the 4090!)");
+      }
     }, 3000);
   });
 }
@@ -34,4 +52,10 @@ walkTheDog()
   .then((gpuThing) => {
     console.log(gpuThing);
     console.log("I've done too much! need some refreshment!");
+  })
+  .catch((rejected) => {
+    console.log(rejected);
+    console.log("You didn't complete the tasks!");
   });
+
+//this is the end of part ONE
