509ce7f191610d5b8505432a5dd6f578944b4f8c
Ash digit <ashdigit44@gmail.com>
2024-11-04 23:45:03 +0000

    came up with a solution for the task but not commpleted

--- Integer_to_Roman.js
+++ Integer_to_Roman.js
@@ -1,3 +1,17 @@
-const Integer_to_Roman = function(){
-    
+const Integer_to_Roman = function(num){
+    const splited = num.toString().split("")
+    let fiqures = []
+
+    for(let i = 0; i < splited.length; i++){
+        // console.log(splited[i])
+        for(let j = splited.length; j > i; j--){
+            console.log(j , "<---J")
+        }
+
+        
+        // fiqures.append(`${splited[i]}${zeros}`)
+    }
+   
 }
+
+Integer_to_Roman(273)
