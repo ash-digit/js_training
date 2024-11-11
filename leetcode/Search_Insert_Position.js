f91f2b8c52c8ca4bdbeae90d1c627e1b66d57e2c
Ash digit <ashdigit44@gmail.com>
2024-11-11 12:02:55 +0000

    the search insert position now passes all the leetCode tests

--- leetcode/Search_Insert_Position.js
+++ leetcode/Search_Insert_Position.js
@@ -1,26 +1,44 @@
 var searchInsert = function(nums, target) {
-    if(target === 0 || nums.indexOf(target + 1 === 1)){
-        return 0;
-    }
     if(nums.indexOf(target) != -1){
+        console.log(target, "already exists")
         return nums.indexOf(target)
-    }else if(target < nums[nums.length - 1]){
-        let x = 0;
-        if(nums.indexOf(target - 1) != -1){
-            x = 1;
-        }
-        const indexOfInsertion = nums.indexOf(target + 1 )
-        if(indexOfInsertion === -1 || indexOfInsertion === 0){
-            return 1 + x
-        }
-        return indexOfInsertion;
-    }else{
+    }else if(target < nums[nums.length - 1]){// if the target in smaller than the last number in the nums array
+       if(nums[nums.indexOf(target - 1)] || target < nums[0]){ // to the left
+        console.log("to the left")
+        return nums.indexOf(target - 1) + 1 
+       }else if(nums[nums.indexOf(target + 1)]){ // to the right
+        console.log("to the right")
+        return nums.indexOf(target + 1)
+       }
+       
+       
+    }else{ // if the target is bigger than the last number in the numbs array
         nums.push(target)
+        console.log("a push needed")
         return nums.indexOf(target)
     }
     
 };
 
 const nums1 = [3,4,9,10]
-// const nums2 = [1,2,6];//target 4
 console.log(searchInsert(nums1, 5))
+
+console.log("-----------------------")
+const nums2 = [3,4,9,10]
+console.log(searchInsert(nums1, 2))
+
+console.log("-----------------------")
+const nums3 = [3,4,9,10]
+console.log(searchInsert(nums1, 12))
+
+console.log("-----------------------")
+const nums4 = [3,4,9,10]
+console.log(searchInsert(nums1, 0))
+
+console.log("-----------------------")
+const nums5 = [5,6,8,10]
+console.log(searchInsert(nums5, 1))
+
+console.log("-----------------------")
+nums6 = [-1,3,5,6];
+console.log(searchInsert(nums6, 0))
