85e4d86f865ae6929b3e080bf06739949a9a0db7
Ash digit <ashdigit44@gmail.com>
2024-11-12 21:22:48 +0000

    3sum passes all the leetcode's tests

--- leetcode/3sum.js
+++ leetcode/3sum.js
@@ -1,25 +1,35 @@
 var threeSum = function(nums) {
-    const sorted = nums.sort();
+    nums.sort((a, b) => a - b);  // Sort numerically
     const res = [];
 
-    for(let i = 0; i < sorted.length; i++){
-        for(let j = 1; j < sorted.length; j++){
-            for(let k = sorted.length; k > 0; k--){
-                let temp = (sorted[i]) + (sorted[j] + ((sorted[k])));
-                let tempArr = [];
-                if(temp === 0){
-                    tempArr.push(sorted[i])
-                    tempArr.push(sorted[j])
-                    tempArr.push(sorted[k])
-                    for(let l = 0; l < res.length; l++){
-                        
-                    }
-                    res.push(tempArr)
-                }
+    for (let i = 0; i < nums.length - 2; i++) {
+        // Skip duplicate values to avoid repeated triplets
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right];
+
+            if (sum === 0) {
+                res.push([nums[i], nums[left], nums[right]]);
+                
+                // Move left and right pointers to avoid duplicates
+                while (left < right && nums[left] === nums[left + 1]) left++;
+                while (left < right && nums[right] === nums[right - 1]) right--;
+
+                left++;
+                right--;
+            } else if (sum < 0) {
+                left++;  // Increase sum by moving left pointer to the right
+            } else {
+                right--; // Decrease sum by moving right pointer to the left
             }
         }
     }
-    return(res)
+
+    return res;
 };
 
 
