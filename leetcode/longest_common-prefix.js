ce62ed8b7d69455a0f3c7def4a3a99c6adda7a55
Ash digit <ashdigit44@gmail.com>
2025-02-22 17:21:02 +0000

    half way to solve longest common prefix

---
+++ leetcode/longest_common-prefix.js
@@ -0,0 +1,11 @@
+let input = ['flower', 'flow', 'flight'];
+let temp = ['a', 'ab', 'abc', 'b']
+let temp2 = []
+
+let longestCommenPrefix = function(arr){
+    if(arr.length === 0) return '';
+    let arrSorted = arr.sort((a,b) =>  b.length - a.length)
+    
+}
+
+longestCommenPrefix(temp2)
