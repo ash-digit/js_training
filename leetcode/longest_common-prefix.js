ca3206c562da46170beab5457578a50209fa471e
Ash digit <ashdigit44@gmail.com>
2025-02-23 18:53:57 +0000

    completed the longest common prefix

--- leetcode/longest_common-prefix.js
+++ leetcode/longest_common-prefix.js
@@ -2,10 +2,19 @@ let input = ['flower', 'flow', 'flight'];
 let temp = ['a', 'ab', 'abc', 'b']
 let temp2 = []
 
-let longestCommenPrefix = function(arr){
-    if(arr.length === 0) return '';
-    let arrSorted = arr.sort((a,b) =>  b.length - a.length)
-    
+let longestCommenPrefix = function(arr){//using vertical comparison
+    let prefix = ''
+    if(arr.length === 0) return prefix;
+
+    for(let i = 0; i < arr[0].length; i++){//Loop through the first string's characters in the array
+        const character = arr[0][i];//Assigning the current character to the character variable
+        for(let j = 0; j < arr.length; j++){//Loop through all elements of arr
+            if (arr[j][i] !== character) return prefix//
+        }
+        prefix = prefix + character
+    }
+    return prefix;
+
 }
 
-longestCommenPrefix(temp2)
+console.log(longestCommenPrefix(input))
